36329a8470b5d1b2662a1317e075aa1d4356e7ff
fernando-meira <thomwork@outlook.com>
2021-08-12 17:21:02 -0300

    feat: 🎸 Added home title and limited main size to 1366px

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import * as S from './styles';
 export function Home() {
   return (
     <S.Container>
+      <h1>Bem-vindo ao Lista Rango</h1>
+
       <Search />
     </S.Container>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.main`
+  margin: 0 auto;
+  max-width: 1366px;
+  padding: 0 4.7rem;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    margin: 2rem 0;
+
+    font-weight: 500;
+    font-size: 1.5rem;
+  }
+`;
