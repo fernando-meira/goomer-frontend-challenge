7c50805b897d2cefe472714c94d639eff799b7a2
fernando-meira <thomwork@outlook.com>
2021-08-12 17:12:52 -0300

    feat: 🎸 Created Search component
    Created component and called within the home page for testing.

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,27 @@
+import { useRef } from 'react';
+
+import searchIcon from 'assets/images/svg/search.svg';
+
+import * as S from './styles';
+
+interface SearchProps {
+  backgroundColor?: string;
+}
+
+export function Search({ backgroundColor }: SearchProps) {
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  return (
+    <S.Container backgroundColor={backgroundColor}>
+      <S.LabelWrapper onClick={() => searchInput.current?.focus()}>
+        <span>Buscar estabelecimento</span>
+      </S.LabelWrapper>
+
+      <input ref={searchInput} type="text" />
+
+      <button type="button">
+        <img src={searchIcon} alt="Magnifying glass" />
+      </button>
+    </S.Container>
+  );
+}
---
+++ src/components/Search/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+import { colors } from 'styles/colors';
+
+interface ContainerProps {
+  backgroundColor?: string;
+}
+
+export const Container = styled.div<ContainerProps>`
+  width: 52.5rem;
+  height: 2.5rem;
+  border-radius: 2.8rem;
+  box-shadow: 0px 2px 4px ${colors.gray[900]};
+
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor || colors.gray[100]};
+
+  > input {
+    border: none;
+    padding: 0.5rem 1rem;
+
+    flex: 1;
+
+    background-color: transparent;
+  }
+
+  > button {
+    margin: 0.75rem 1.2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const LabelWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 16.25rem;
+  border-radius: 1.5rem 0 0 1.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+  background-color: ${colors.white};
+
+  > span {
+    font-size: 1rem;
+    font-weight: 500;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+`;
