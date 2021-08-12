a340333e2775f79c6ee49b8fe094c0f7b17a3102
fernando-meira <thomwork@outlook.com>
2021-08-12 16:55:25 -0300

    feat: 🎸 Created Header component
    Added Header in App file because it is used on all pages.

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import { colors } from 'styles/colors';
+
+export const Header = styled.header`
+  width: 100vw;
+  height: 3.875rem;
+  box-shadow: 0px 4px 8px ${colors.gray[900]};
+
+  background-color: ${colors.green[500]};
+`;
